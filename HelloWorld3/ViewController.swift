4455ef63f20b2f1f67a2edff04ee21abea11174e
IlyaRakov97 <balonka97@mail.ru>
2022-01-20 10:56:36 +0400

    Button

--- HelloWorld3/ViewController.swift
+++ HelloWorld3/ViewController.swift
@@ -10,14 +10,27 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var helloWorldLabel: UILabel!
     @IBOutlet weak var inscriptionsButton: UIButton!
+    @IBOutlet weak var deleteTextLabel: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        helloWorldLabel.text.
+        helloWorldLabel.layer.isHidden = true
         inscriptionsButton.layer.cornerRadius = 20
+        helloWorldLabel.textColor = .red
+        deleteTextLabel.layer.cornerRadius = 20
         
     }
     @IBAction func inscripnionPressButton() {
+        helloWorldLabel.isHidden.toggle()
+        
+        if helloWorldLabel.isHidden{
+            inscriptionsButton.setTitle("Show text", for: .normal)
+        }
+        else {
+            inscriptionsButton.setTitle("The inscription disappears", for: .normal)
+        }
+    }
+    @IBAction func deleteTextButton() {
     }
     
 
